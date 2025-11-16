232ddd36c9699e382d3cceac0e21e8288aae27c0
Baister <gustavobaister@gmail.com>
2025-11-16 14:30:44 -0300

    Entendi o conceito de função pura

--- Questão2.py
+++ Questão2.py
@@ -1,2 +1,39 @@
 #Fazendo exercícios para melhorar a lógica
 #Transformando funções em uma só
+
+# Exercício 1 — Transformar funções repetidas em uma só
+
+# Crie uma função que recebe:
+
+# dois números
+
+# um operador (+, -, *, /)
+
+# E retorna o resultado.
+
+# Sem if dentro do menu.
+
+def soma(a, b):
+    return a+b
+
+resultado = soma(10,20)
+print(resultado)
+
+def diferenca(a,b):
+    return a-b
+
+resultado = diferenca(10,20)
+print(resultado)
+
+def multiplica(a,b):
+    return a * b
+resultado = multiplica(10,20)
+print(resultado)
+
+def div(a,b):
+    return a/b
+
+resultado = div(10,20)
+print(resultado)
+
+#Esse é o conceito de função pura
